0a7f9cf538e78baec56c061e8d971d3ad60d790d
MdObaidullah629 <hujaifaobaidullah629@gmail.com>
2026-05-03 15:10:29 +0600

    some dynamic process added

--- src/app/api/auth/[...better-auth]/route.js
+++ src/app/api/auth/[...better-auth]/route.js
@@ -1,4 +1,7 @@
 import { auth } from "@/lib/auth";
 import { toNextJsHandler } from "better-auth/next-js";
 
+// এই লাইনটি যোগ করুন যাতে Next.js এটাকে স্ট্যাটিক হিসেবে বিল্ড না করে
+export const dynamic = "force-dynamic"; 
+
 export const { GET, POST } = toNextJsHandler(auth);
